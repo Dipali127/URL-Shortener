fd402004a672d45d080b6fcffc706dff3761e688
Dipali Bohara <dipalibohara500@gmail.com>
2025-11-11 20:26:19 +0530

    add index.js file as application entry point

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+// Middleware to handle JSON data in request body
+app.use(express.json());
+
+// Load environment variables from .env file
+const dotenv = require('dotenv');
+dotenv.config({path: '../.env'});
+const port = process.env.PORT || 3001;
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.clusterString).then(() => { console.log("mongoDB successfully connected") })
+    .catch((error) => { console.log(error.message) });
+
+const redisClient = require('./redisConfig.js');
+redisClient.connect().then(() => { console.log("connected to redis") }).catch((error) => console.log(error.message))
+
+const route = require('./router/routes.js');
+app.use('/', route);
+
+app.listen(port, () => {
+    console.log(`Server listen on the port ${port}`);
+})
+
+
